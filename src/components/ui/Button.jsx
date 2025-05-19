8882019c63e84eedf8d4557f16fe40d9a475595d
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-05-19 23:22:01 +0600

    navbar added and make it full responive

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const Button = ({
-    children,
-    type = 'button',
-    onClick = () => { },
-    variant = 'primary',
-    className = '',
-    ...props
+  children,
+  type = "button",
+  onClick = () => {},
+  variant = "primary",
+  className = "",
+  ...props
 }) => {
-    let styles = 'px-4 py-1.5 rounded font-medium transition duration-200 focus:outline-none ';
+  let styles =
+    "px-4  rounded font-medium transition duration-200 focus:outline-none ";
 
-    if (variant === 'primary') {
-        styles += 'bg-gradient-to-tl from-purple-700 via-indigo-700 to-indigo-600 text-white hover:bg-indigo-500';
-    } else if (variant === 'secondary') {
-        styles += 'bg-white text-indigo-700 hover:bg-indigo-100';
-    } else if (variant === 'outline') {
-        styles += 'border border-indigo-600 text-indigo-600 hover:bg-indigo-50';
-    } else if (variant === 'danger') {
-        styles += 'bg-red-600 text-white hover:bg-red-500';
-    } else {
-        styles += 'bg-gray-200 text-black';
-    }
+  if (variant === "primary") {
+    styles +=
+      "bg-[#f97316] text-white px-4 py-2 rounded-md text-sm hover:bg-[#ea580c]";
+  } else if (variant === "secondary") {
+    styles +=
+      "bg-[#fdf6ee] text-[#1a1a1a] border border-orange-200 hover:bg-orange-100";
+  } else if (variant === "outline") {
+    styles += "border border-[#f97316] py-1 text-[#f97316] hover:bg-orange-100";
+  } else if (variant === "danger") {
+    styles += "bg-red-600 text-white hover:bg-red-500";
+  } else {
+    styles += "bg-gray-200 text-black";
+  }
 
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            className={`${styles} ${className}`}
-            {...props}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${styles} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
